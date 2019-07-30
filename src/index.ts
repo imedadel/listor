c210dc6f835d05ffe15ea240fe4d5aaa198faa14
Imed Adel <imed.adel@sastec-group.com>
2019-07-30 09:51:09 +0100

    fix: add return type

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export const listor = (words:any[], oxford:boolean = true, comma:string = `,`, separator:string = `and`) => {
+export const listor = (words:any[], oxford:boolean = true, comma:string = `,`, separator:string = `and`):string => {
   if (words.length === 1) return words[0].toString()
   else if (words.length === 2) return `${words[0]} ${separator} ${words[1]}`
   else {
